8cf601e0a3e3fdc4bae7b3e1e1cd8f6732b56d3b
Ta <ta@appsynth.net>
2022-03-07 14:46:52 +0700

    feat: accept passing inside children

--- components/SimpleText.js
+++ components/SimpleText.js
@@ -38,14 +38,14 @@ const useSimpleText = () => {
 }
 
 
-const SimpleText = ({ text = 'default text', content = null }) => {
+const SimpleText = ({ text = 'default text', children = null }) => {
     const { isReady } = useSimpleText();
     return isReady && (
         <StyledCard>
             <StyledSimpleText>{text}</StyledSimpleText>
             <StyledParagraph>
-                {content 
-                    ? content 
+                {children 
+                    ? children 
                     : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
             </StyledParagraph>
         </StyledCard>
